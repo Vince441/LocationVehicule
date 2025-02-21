db4b082e9822ff4da7e939e1db2fdcf3d460c07d
v.de.pontlevoye <v.de.pontlevoye@accenture.com>
2025-02-21 12:27:50 +0100

    ajout des constraints dans les ClientDto, AdminDto, AdresseDto

--- src/main/java/com/accenture/service/dto/AdresseDto.java
+++ src/main/java/com/accenture/service/dto/AdresseDto.java
@@ -1,8 +1,16 @@
 package com.accenture.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record AdresseDto(
+        
+        @NotBlank(message = "La rue est obligatoire")
         String rue,
+
+        @NotBlank(message = "Le code postal est obligatoire")
         String codePostal,
+
+        @NotBlank(message = "La ville est obligatoire")
         String ville
 
 ) {
--- src/main/java/com/accenture/service/dto/ClientRequestDto.java
+++ src/main/java/com/accenture/service/dto/ClientRequestDto.java
@@ -1,16 +1,32 @@
 package com.accenture.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 public record ClientRequestDto(
-
+        @NotBlank(message = "Le nom est obligatoire")
         String nom,
+
+        @NotBlank(message = "Le prenom est obligatoire")
         String prenom,
+
+        @NotBlank(message = "L'email est obligatoire")
         String email,
+
+        @NotBlank(message = "Le password est obligatoire")
         String password,
+
+        @NotBlank(message = "L'adresse est obligatoire")
         AdresseDto adresse,
+
+        @NotNull(message = "La date de naissance est obligatoire")
         LocalDate dateDeNaissance,
+
+        @NotNull(message = "La date d'inscription est obligatoire")
         LocalDate dateInscription,
+
         String permis,
         Boolean desactive
 
